38a66916252bf2160c718205558c312045b10aed
Arpi Derm <alexwright@Arpis-MacBook-Pro.local>
2025-05-11 09:50:00 -0700

    Setting up model for ABM

--- proxima_model/tools/logger.py
+++ proxima_model/tools/logger.py
@@ -1,46 +1,54 @@
-import pandas as pd
 import sys
 import os
-
-
-def save_time_series_data_to_file(dataframes, filename):
-    """
-    Appends a list of pandas DataFrames and saves the combined DataFrame to a CSV file.
-
-    Args:
-        dataframes (list of pd.DataFrame): List of pandas DataFrames to be appended.
-        filename (str): The name of the CSV file to save the combined DataFrame.
-    """
-    combined_df = pd.concat(dataframes, axis=1).reset_index(drop=True)
-    combined_df.to_csv(filename, index=False)
-
 
 class Logger:
     def __init__(self, filename=None, disable_print=False):
         self.filename = filename
         self.disable_print = disable_print
         self.default_stdout = sys.stdout
+        self.file = None  # Track the open file explicitly
 
     def __enter__(self):
         if self.disable_print:
-            sys.stdout = open(os.devnull, "w")
+            self.file = open(os.devnull, "w")
         elif self.filename:
-            sys.stdout = open(self.filename, "w")
+            self.file = open(self.filename, "w")
+        else:
+            self.file = None
+        
+        if self.file:
+            sys.stdout = self.file
+        
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.stdout.close()
-        sys.stdout = self.default_stdout
+        if self.file:
+            sys.stdout.flush()
+            sys.stdout = self.default_stdout
+            self.file.close()
+            self.file = None
+        else:
+            sys.stdout = self.default_stdout
 
     def enable(self):
         sys.stdout = self.default_stdout
 
     def disable(self):
-        sys.stdout = open(os.devnull, "w")
+        self._close_file()
+        self.file = open(os.devnull, "w")
+        sys.stdout = self.file
 
     def set_file(self, filename):
-        sys.stdout = open(filename, "w")
+        self._close_file()
+        self.file = open(filename, "w")
+        sys.stdout = self.file
 
     def reset(self):
-        sys.stdout.close()
+        self._close_file()
         sys.stdout = self.default_stdout
+
+    def _close_file(self):
+        if self.file:
+            sys.stdout.flush()
+            self.file.close()
+            self.file = None
